16b7e6d882e02d70b13c4dd596cbfb67e62e4b79
Can Abacigil <kmbby@users.noreply.github.com>
2016-08-16 19:56:44 +0300

    - update

--- config/files.js
+++ config/files.js
@@ -7,10 +7,10 @@ module.exports.files = {
   // Defaults to 500MB
   maxBytes: 524288000,
   adapter: require('skipper-s3'),
-  key: process.env.S3_API_KEY,
-  secret: process.env.S3_API_SECRET,
-  bucket: process.env.S3_BUCKET,
-  region: process.env.S3_REGION || undefined,
+  key: 'AKIAITHHFZYBSIAWYZ6Q',
+  secret: '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',
+  bucket: 'io.teamsql.version',
+  region: 'us-east-1',
   endpoint: process.env.S3_ENDPOINT || undefined,
   token: process.env.S3_TOKEN || undefined
 };
