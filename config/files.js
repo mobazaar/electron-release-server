7ef9a14f9cd505be469607b09a505fd9a5169475
Can Abacigil <abacigil@gmail.com>
2016-08-16 19:46:36 +0300

    - bug fix

--- config/files.js
+++ config/files.js
@@ -6,10 +6,11 @@ module.exports.files = {
   // Maximum allowed file size in bytes
   // Defaults to 500MB
   maxBytes: 524288000,
-  key: sails.config.files.key,
-  secret: sails.config.files.secret,
-  bucket: sails.config.files.bucket,
-  region: sails.config.files.region,
-  endpoint: undefined,
-  token: undefined
+  adapter: require('skipper-s3'),
+  key: process.env.S3_API_KEY,
+  secret: process.env.S3_API_SECRET,
+  bucket: process.env.S3_BUCKET,
+  region: process.env.S3_REGION || undefined,
+  endpoint: process.env.S3_ENDPOINT || undefined,
+  token: process.env.S3_TOKEN || undefined
 };
