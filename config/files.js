e00d57b7a6e09c74bd7da78386641d23fcf15a27
Can Abacigil <abacigil@gmail.com>
2016-08-17 21:42:02 +0300

    bug fix

--- config/files.js
+++ config/files.js
@@ -5,12 +5,5 @@
 module.exports.files = {
   // Maximum allowed file size in bytes
   // Defaults to 500MB
-  maxBytes: 524288000,
-  adapter: require('skipper-s3'),
-  key: 'AKIAITHHFZYBSIAWYZ6Q',
-  secret: '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',
-  bucket: 'io.teamsql.version',
-  region: 'us-east-1',
-  endpoint: process.env.S3_ENDPOINT || undefined,
-  token: process.env.S3_TOKEN || undefined
+  maxBytes: 524288000
 };
